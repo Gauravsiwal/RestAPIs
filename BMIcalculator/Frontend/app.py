1c60a042193dc8222d9421ec973c1bd5414dfa29
Gaurav Siwal <78802276+Gauravsiwal@users.noreply.github.com>
2026-05-24 00:50:41 +0530

    add url

--- BMIcalculator/Frontend/app.py
+++ BMIcalculator/Frontend/app.py
@@ -7,7 +7,7 @@ height = st.number_input('Enter your height in cms:')
 weight = st.number_input('Enter your weight in kgs:')
 
 if st.button('My BMI'): 
-    response = requests.get("http://127.0.0.1:8000/bmi",
+    response = requests.get("https://restapis-m9t8.onrender.com//bmi",
                         params={"weight":weight,"height":height})
     data = response.json()
     st.success(f"BMI: {data['bmi']}")
